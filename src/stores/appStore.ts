92ae4f981a3d67b394a64070ca9f3db4cf64ed7a
hyochan <dooboolab@gmail.com>
2018-03-01 18:42:02 +0900

    Initial commit from dooboo init from dooboo-cli.

---
+++ src/stores/appStore.ts
@@ -0,0 +1,60 @@
+import User from '@models/User';
+import { observable } from 'mobx';
+
+import moment from 'moment';
+import 'moment/locale/ko';
+import 'moment/locale/ja';
+// import 'moment/locale/zh-cn';
+// import 'moment/locale/es';
+// import 'moment/locale/fr';
+// import 'moment/locale/id';
+
+class ObservableListStore {
+  @observable
+  private rootNavigator: any;
+  @observable
+  private rootNavigatorActionHorizontal: boolean;
+  @observable private rootNavigatorParams: object;
+  @observable private user: User;
+
+  constructor() {
+    this.rootNavigatorActionHorizontal = false;
+    this.rootNavigatorParams = {};
+    this.user = new User();
+  }
+
+  public get $rootNavigator(): any  {
+    return this.rootNavigator;
+  }
+
+  public set $rootNavigator(value: any ) {
+    this.rootNavigator = value;
+  }
+
+  public get $rootNavigatorActionHorizontal(): boolean  {
+    return this.rootNavigatorActionHorizontal;
+  }
+
+  public set $rootNavigatorActionHorizontal(value: boolean ) {
+    this.rootNavigatorActionHorizontal = value;
+  }
+
+  public get $rootNavigatorParams(): any  {
+    return this.rootNavigatorParams;
+  }
+
+  public set $rootNavigatorParams(value: any ) {
+    this.rootNavigatorParams = value;
+  }
+
+  public get $user(): User {
+    return this.user;
+  }
+
+  public set $user(value: User) {
+    this.user = value;
+  }
+}
+
+const observableListStore = new ObservableListStore();
+export default observableListStore;
