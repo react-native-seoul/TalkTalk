c9f938239b03bf46587e4c2857dfa280d3339f1f
hyochan <dooboolab@gmail.com>
2018-10-04 02:58:57 +0900

    Upgraded to react-native@0.57.

--- src/components/navigation/MainStackNavigator.tsx
+++ src/components/navigation/MainStackNavigator.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { AsyncStorage, View, Platform, StyleSheet } from 'react-native';
 import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 // import CardStackStyleInterpolator from 'react-navigation/src/views/CardStack/CardStackStyleInterpolator';
-import StackViewStyleInterpolator from 'react-navigation/src/views/StackView/StackViewStyleInterpolator';
+import StackViewStyleInterpolator from 'react-navigation-stack/dist/views/StackView/StackViewStyleInterpolator';
 
 import MainTabNavigator, {MainTabNavigationOptions} from './MainTabNavigator';
 import Login from '@screen/Login';
